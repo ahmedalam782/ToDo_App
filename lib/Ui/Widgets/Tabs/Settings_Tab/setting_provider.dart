4135bfe2e396ff34db015a503d6a63fb03d263ca
ahmedalam <ahmedalam4887482@gmail.com>
2024-08-23 08:55:09 +0300

    create login and register and connect every user with his task when login will save data and will open home

--- lib/Ui/Widgets/Tabs/Settings_Tab/setting_provider.dart
+++ lib/Ui/Widgets/Tabs/Settings_Tab/setting_provider.dart
@@ -25,16 +25,4 @@ class SettingProvider extends ChangeNotifier {
     await CacheHelper.saveData(key: 'isLanguage', value: lang);
     notifyListeners();
   }
-
-  Future<void> getThemeMode() async {
-    themeMode = await CacheHelper.getData(key: 'isDark') == null
-        ? ThemeMode.light
-        : CacheHelper.getData(key: 'isDark')
-            ? ThemeMode.dark
-            : ThemeMode.light;
-  }
-
-  Future<void> getLang() async {
-    lang = await CacheHelper.getData(key: 'isLanguage') ?? "en";
-  }
 }
